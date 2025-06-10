bfc457e86ae4f6b9a78b37b9165f145f165bf4cd
Aibek <aibek.aibek.1204@gmail.com>
2025-06-10 11:11:39 +0500

    Final 10.06 Испрвление news_portal/urls.py

--- news_portal/urls.py
+++ news_portal/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
