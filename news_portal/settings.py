1e5b5cfdb550a4da3aab6feb2cf2d2e197f5ed4d
Aibek <aibek.aibek.1204@gmail.com>
2025-06-13 05:44:34 +0500

    Удаленный requirements.txt

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -3,13 +3,17 @@ from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 
+# Загрузка переменных из файла .env
 load_dotenv()
 
+# Основная директория проекта
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Секретный ключ и дебаг-режим из переменных окружения
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+# Разрешенные хосты
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -18,7 +22,7 @@ ALLOWED_HOSTS = [
     'www.info-kaz.kz',
     'info-kaz-app-wsool.ondigitalocean.app',
 ]
-A = 123131
+
 # Приложения
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -50,7 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'news_portal.urls'
 
-# Templates
+# Шаблоны
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -70,12 +74,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'news_portal.wsgi.application'
 
-# База данных
+# Настройки базы данных через переменные окружения
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,  # Максимально долгое соединение (в секундах)
-        ssl_require=True  # Явный запрос SSL
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
@@ -93,20 +97,19 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Статика
+# Статические файлы
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Медиа — локальное хранилище для изображений/видео
+# Медиа-файлы
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Файловое хранилище
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-# Summernote — локальное хранилище
-from news_portal.config import ADMIN_URL_PREFIX
+# Конфигурация Summernote
 SUMMERNOTE_CONFIG = {
     'attachment_storage_class': 'django.core.files.storage.FileSystemStorage',
     'attachment_filesize_limit': 10 * 1024 * 1024,  # 10MB
